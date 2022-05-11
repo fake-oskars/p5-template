55f24e144ef1a7492631729b92a1f1086cfc6e6f
fake-oskars <51405688+fake-oskars@users.noreply.github.com>
2022-05-11 10:39:33 +0300

    cleaner. eq+cursor

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -22,15 +22,7 @@ export const Sketch = (p: p5) => {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
 
-  // p.draw = () => {
-  //   p.background(0);
-  //   p.fill([100, 210, 254]);
-  //   p.circle(w / 2, h / 2, 50);
-  //   p.fill([200, 210, 254]);
-  //   p.circle(w / 2+100, h / 2, 50);
-  //   p.fill([200, 210, 154]);
-  //   p.circle(w / 2-100, h / 2, 50);
-    
+
 
 // pavairo apļus kodā
     // for (let solis = 0; solis<3; solis +=1 ){
@@ -63,12 +55,10 @@ export const Sketch = (p: p5) => {
 
 p.stroke([0,0,0])
     
-    p.translate(w /2, h/2); 
-   p.rotate(step/60);
+
     for (let i = 0; i < circleLow.length; i += 1){
  
-p.circle(circleLow.length -i,0, circleLow[i] *2);
-//p.circle(w/2- circleLow.length + i, h/2, circleLow[i] *2);
+p.circle(w/2- circleLow.length + i +250, h/2, circleLow[i] *1);
 
 
     }
@@ -77,22 +67,17 @@ p.circle(circleLow.length -i,0, circleLow[i] *2);
 /*     p.circle(step, h/2, low * 3);
     p.circle(step, h/2+100, mid * 3);
     p.circle(step, h/2+200, high * 3);
- */    step += 1;
+ */   
+
+    step += 1;
     p.mouseMoved = (event: MouseEvent) => {
-      //p.fill([200, 210, 154]);
-     // p.circle(event.clientX, event.clientY, 50);
-    //p.stroke([0,0,0])
       let levels = fft.getValue();
-  
-     // const low = eq(levels, 0, 0.3);
       const mid = eq(levels, 0.3, 0.6);
-     // const high = eq(levels, 0.6, 1);
 
 
   //KURSORS
       p.circle(event.clientX, event.clientY, mid * 3);
-     // p.circle(w / 2 + 40, h / 2, mid * 3);
-     // p.circle(w / 2 + 80, h / 2, high * 3);
+
     }
 
     if (circleLow.length>500){
